30f15f6e910e4558ff24c5247df0a589c2a91d37
oguz <oguzalpcetin@marun.edu.tr>
2024-08-01 21:37:13 +0300

    [UPDATE]: ex01 is updated.

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -1,15 +1,59 @@
 #ifndef ITER_HPP
 # define ITER_HPP
 
+# include <iostream>
+
 template <typename T>
-void func(T &elem) {
-	elem += 3;
+void f1(T &elem) { elem += 3; }
+
+template <typename T, int INC = 3>
+struct s_fun{
+	void operator()(T &elem) { elem += INC; }
+};
+
+template <template<typename> class Func, typename T>
+void iter(T *ptr, int N, Func<T> fnc){
+	for (int i = 0; i < N; i++)
+		fnc(ptr[i]);
 }
 
 template <typename T>
-void iter(T *ptr, size_t N, (*f)(T)){
-	for (size_t i = 0; i < N; i++)
-		f(ptr[i]);
+void iter(T *ptr, int N, void (*fnc)(T &)){
+	for (int i = 0; i < N; i++)
+		fnc(ptr[i]);
+}
+
+template <typename T>
+void iter(T &ref, int N, void (*fnc)(T &)){
+	for (int i = 0; i < N; i++)
+		fnc(ref);
+}
+
+template <typename T>
+void iter(T *ptr, int N, void (*fnc)(T const &)){
+	for (int i = 0; i < N; i++)
+		fnc(ptr[i]);
+}
+
+
+template <template <typename,int> class Func, typename T, int INC>
+void iter(T *ptr, int N, Func<T, INC> fnc){
+	for (int i = 0; i < N; i++)
+		fnc(ptr[i]);
+}
+
+// --------------------------------------------------------
+void my_toupper(std::string &s) {
+	for (size_t i = 0; i < s.length(); i++) {
+		s[i] = std::toupper(s[i]);
+	}
+}
+
+template <typename T>
+void print_arr(T *arr, int size) {
+	for (int i = 0; i < size; i++)
+		std::cout << arr[i] << " ";
+	std::cout << std::endl;
 }
 
 #endif
--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -1,7 +1,65 @@
-#include <stdio.h>
+#include <iostream>
 #include "iter.hpp"
 
-int	main() {
-	int i_arr[5] = {1,2,3,4,5};
+class Awesome {
+	public:
+		Awesome( void ) : _n( 42 ) { return; }
+		int get( void ) const { return this->_n; }
+	private:
+		int _n;
+};
+std::ostream & operator<<( std::ostream & o, Awesome const & rhs ) { o << rhs.get(); return o; }
 
+template< typename T >
+void print( T const & x ) { std::cout << x << std::endl; return; }
+
+
+void my_toupper(std::string &s);
+
+void stringLower(std::string &s) {
+	for (size_t i = 0; i < s.length(); i++)
+		s[i] = std::tolower(s[i]);
+}
+
+
+int	main() {
+
+	int i_arr[5] = {1,2,3,4,5};
+	const int arr_size = sizeof(i_arr) / sizeof(i_arr[0]);
+
+	const unsigned int increment = 10;
+
+	std::cout << "--------------------> 1" << std::endl;
+	s_fun<int, increment> f_i;
+	iter(i_arr, arr_size, f_i);	// iter<s_fun, int, 10>(i_arr, size, f_i);
+	print_arr(i_arr, arr_size);
+	
+	std::cout << "--------------------> 2" << std::endl;
+	iter(i_arr, arr_size, s_fun<int, increment>());	// iter<s_fun, int, 10>(i_arr, size, s_fun<int, increment>())
+	print_arr(i_arr, arr_size);
+
+	std::cout << "--------------------> 3" << std::endl;
+	std::string s_arr[5] = {"Lorem Ipsum", "is", "simply", "dummy", "text"};
+	const int s_size = sizeof(s_arr)/sizeof(s_arr[0]);
+	iter(s_arr, s_size, my_toupper);
+	print_arr(s_arr, s_size);
+
+	std::cout << "--------------------> 4" << std::endl;
+	print_arr(s_arr, s_size);
+	s_fun<char *, 32> f_s;
+	for (size_t i = 0; i < s_size; i++)
+		iter(s_arr, 2, stringLower);
+	print_arr(s_arr, s_size);
+
+	std::cout << "--------------------> EVO TEST" << std::endl;
+	int tab[] = { 0, 1, 2, 3, 4 };
+	Awesome tab2[5];
+
+	iter( tab, 5, print );
+	iter( tab2, 5, print );
+	std::cout << "------------------" << std::endl;
+
+
+
+	return (0);
 }
